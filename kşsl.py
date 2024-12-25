de6b19c8ceb15a60e1b909395a0310c05e7a0712
ALPA58 <aslanalipusat8@gmail.com>
2024-12-25 12:59:46 +0200

    Create kşsl.py

---
+++ kşsl.py
@@ -0,0 +1,5 @@
+kisisel_bilgileri = ["osman","karaosman","14","168"]
+egitim_bilgileri = ["Necla Görer","Ş.A.T.O","B.A.K"]
+adres_bilgileri = ["Güzelyurt 2102 sok.","Küçük Çekmece kanarya","beylikdüzü"]
+
+print(kisisel_bilgileri,egitim_bilgileri,adres_bilgileri)
